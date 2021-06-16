dfc8cb7e5d480d1c51d17dc606a4af033caa79ed
lymarrie <lucmarrie1@gmail.com>
2021-06-16 16:06:43 -0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    purge: [
+      './partials/**/*.hbs', 
+      './pages/**/*.hbs',
+      './templates/**/*.hbs',
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
